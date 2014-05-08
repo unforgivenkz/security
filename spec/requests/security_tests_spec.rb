423d35808b1b62bb93904da3bc142187c616cb39
Duyunova Vika <v-duyunova@yandex.ru>
2014-05-08 23:12:12 +0600

    All pages - title

--- spec/requests/security_tests_spec.rb
+++ spec/requests/security_tests_spec.rb
@@ -6,12 +6,20 @@ describe "SecurityTests" do
       visit '/oib/home'
       expect(page).to have_content('ДБиЗИ')
     end
+    it "should have the right title" do
+      visit '/oib/home'
+      expect(page).to have_title("ДБиЗИ | Главная")
+    end
   end
 
 describe "News page" do
     it "should have the content 'нужно соблюдать'" do
       visit '/oib/news'
       expect(page).to have_content('нужно соблюдать')
+    end
+    it "should have the right title" do
+      visit '/oib/news'
+      expect(page).to have_title("ДБиЗИ | Новости")
     end
   end
 
@@ -20,6 +28,10 @@ describe "Structure page" do
       visit '/oib/structure'
       expect(page).to have_content('команда')
     end
+    it "should have the right title" do
+      visit '/oib/structure'
+      expect(page).to have_title("ДБиЗИ | Структура")
+    end
   end
 
 describe "Help page" do
@@ -27,12 +39,20 @@ describe "Help page" do
       visit '/oib/help'
       expect(page).to have_content('База')
     end
+    it "should have the right title" do
+      visit '/oib/help'
+      expect(page).to have_title("ДБиЗИ | Скорая помощь")
+    end
   end
 
 describe "Room page" do
     it "should have the content 'Поговорим'" do
       visit '/oib/room'
       expect(page).to have_content('Поговорим')
+    end
+    it "should have the right title" do
+      visit '/oib/room'
+      expect(page).to have_title("ДБиЗИ | Курилка")
     end
   end  
 end
