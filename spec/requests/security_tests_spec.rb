dc170f085820f0a1aee3ab6c4579a37afc395c60
Duyunova Vika <v-duyunova@yandex.ru>
2014-05-08 22:52:53 +0600

    All pages

--- spec/requests/security_tests_spec.rb
+++ spec/requests/security_tests_spec.rb
@@ -7,4 +7,32 @@ describe "SecurityTests" do
       expect(page).to have_content('ДБиЗИ')
     end
   end
+
+describe "News page" do
+    it "should have the content 'нужно соблюдать'" do
+      visit '/oib/news'
+      expect(page).to have_content('нужно соблюдать')
+    end
+  end
+
+describe "Structure page" do
+    it "should have the content 'команда'" do
+      visit '/oib/structure'
+      expect(page).to have_content('команда')
+    end
+  end
+
+describe "Help page" do
+    it "should have the content 'База'" do
+      visit '/oib/help'
+      expect(page).to have_content('База')
+    end
+  end
+
+describe "Room page" do
+    it "should have the content 'Поговорим'" do
+      visit '/oib/room'
+      expect(page).to have_content('Поговорим')
+    end
+  end  
 end
