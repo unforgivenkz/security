9adb9d55e66d99965d5ea597f2fbb67c5a54b996
Duyunova Vika <v-duyunova@yandex.ru>
2014-05-19 22:52:48 +0600

    Signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,13 @@
 module UsersHelper
 
 def login()
-	base_login = request.env['HTTP_REMOTE_USER']
-	#if base_login.nil?
-	#	base_login = ENV['USER']
-	#end
-	#if ENV['USERNAME'].nil? && ENV['USER'].nil?
-	#	base_login = request.remote_ip
-	#end
+	base_login = ENV['USERNAME']
+	if base_login.nil?
+		base_login = ENV['USER']
+	end
+	if ENV['USERNAME'].nil? && ENV['USER'].nil?
+		base_login = request.env['REMOTE_ADDR']
+	end
 "#{base_login}"
 end
 
