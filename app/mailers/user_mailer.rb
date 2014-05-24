0186c4fc3310b0f661fb2408820c93810f6739ab
Duyunova Vika <v-duyunova@yandex.ru>
2014-05-24 14:00:01 +0600

    Send mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: 'notifications@example.com'
+  default from: 'dbzi-portal@sberbank.kz'
 
    def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email, subject: 'Добро пожаловать!')
   end
   
 end
