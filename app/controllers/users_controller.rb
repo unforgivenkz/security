cab50036ea95f45be2ef417cc04e27938976f4ea
Duyunova Vika <v-duyunova@yandex.ru>
2014-06-29 13:01:09 +0600

    all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 
   def index
@@ -55,6 +55,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Пользователь удален."
+    redirect_to users_url
+  end
+
   private
 
   def user_params
@@ -75,6 +81,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140527205308) do
+ActiveRecord::Schema.define(version: 20140629063425) do
 
   create_table "departments", force: true do |t|
     t.string   "surname"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20140527205308) do
     t.string   "remember_token"
     t.string   "sign_token"
     t.boolean  "active"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
