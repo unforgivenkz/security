3ef1272a2f61971b207bf3afab7d0a36811d953e
Duyunova Vika <v-duyunova@yandex.ru>
2014-07-31 21:53:53 +0600

    user-microposts-done

--- app/controllers/oib_controller.rb
+++ app/controllers/oib_controller.rb
@@ -16,6 +16,8 @@ class OibController < ApplicationController
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 30)
     end
+
+    
   end
 
 end
