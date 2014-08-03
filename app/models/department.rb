4bf7997c570f1c8a87da1adfe8a0dbec3f9278ea
Duyunova Vika <v-duyunova@yandex.ru>
2014-08-03 21:36:41 +0600

    structure-done

--- app/models/department.rb
+++
@@ -1,5 +0,0 @@
-class Department < ActiveRecord::Base
-	validates :name1, presence: true, length: { maximum: 51}
-	validates :name2, presence: true, length: { maximum: 51}
-	validates :name3, presence: true, length: { maximum: 51}
-end
